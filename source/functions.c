ad1e3594194f1283ae00f23d6eb7da942824b4a5
Mohammed D. Belgoumri <mohammedbelgoumri@gmail.com>
2021-06-28 15:27:46 +0100

    reduce numer of tabs in help

--- source/functions.c
+++ source/functions.c
@@ -2,12 +2,12 @@
 
 void printHelp()
 {
-    printf("-h --help\t\t\t\tPrint this help text and exit.\n");
-    printf("-v --version\t\t\t\tPrint program version and exit.\n");
-    printf("-0\t\t\t\t\tLook only in directory.\n");
-    printf("-1\t\t\t\t\tLook in directory and its immediate children\n");
-    printf("-n\t\t\t\t\tLook in directory and all its children of depth =< n.\n");
-    printf("-r\t\t\t\t\tLook recursively in directory and its children.\n");
+    printf("-h --help\t\tPrint this help text and exit.\n");
+    printf("-v --version\t\tPrint program version and exit.\n");
+    printf("-0\t\t\tLook only in directory.\n");
+    printf("-1\t\t\tLook in directory and its immediate children\n");
+    printf("-n\t\t\tLook in directory and all its children of depth =< n.\n");
+    printf("-r\t\t\tLook recursively in directory and its children.\n");
 }
 
 
