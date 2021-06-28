a8e2284f37db60083958e97804eced0162067227
Mohammed D. Belgoumri <mohammedbelgoumri@gmail.com>
2021-06-28 15:29:21 +0100

    primitive option handling

--- source/search.c
+++ source/search.c
@@ -2,7 +2,54 @@
 
 int main(int argc, char *argv[])
 {
-    char c;
-    printVersion();
-    return 0;
+    int c;
+    
+    while(1)
+    {
+        int opt_index = 0;    
+        static struct option long_options[] = {
+            {"all",          no_argument, NULL, 'a'},
+            {"date",         no_argument, NULL, 'd'},
+            {"help",         no_argument, NULL, 'h'},
+            {"modification", no_argument, NULL, 'm'},
+            {"protection",   no_argument, NULL, 'p'},
+            {"size",         no_argument, NULL, 's'},
+            {"type",         no_argument, NULL, 't'},
+            {"version",      no_argument, NULL, 'v'},
+            {NULL,           0,           NULL,  0}
+        };
+        c = getopt_long(argc, argv, ":adhmprstv", long_options, &opt_index);
+        
+        if (c == -1)
+            break;
+        
+        switch(c)
+        {
+            case 'a': 
+                break;
+            case 'd': 
+                break;
+            case 'h':
+                printHelp();
+                break;
+            case 'p': 
+                break;
+            case 'r': 
+                break;
+            case 's': 
+                break;
+            case 't': 
+                break;
+            case 'v':
+                printVersion();
+                break;
+            case '?':
+                printf("Unknown option %c\n", optopt);
+                break;
+            default:
+                printf("?? getopt returned character code 0%d ??\n", c);
+                break;
+        }
+    }
+    return EXIT_SUCCESS;
 }
