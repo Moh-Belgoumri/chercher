bcf7baefae4e4a8df9d299ffefeef90ff1a872a9
Mohammed D. Belgoumri <mohammedbelgoumri@gmail.com>
2021-07-01 19:50:59 +0100

    #include <fnmatch.h>

--- include/functions.h
+++ include/functions.h
@@ -1,13 +1,14 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/wait.h>
-#include<readline/readline.h>
-#include<readline/history.h>
-#include<getopt.h>
-#include<sys/stat.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <getopt.h>
+#include <sys/stat.h>
+#include <fnmatch.h>
 
 void printHelp();
 void printVersion();
