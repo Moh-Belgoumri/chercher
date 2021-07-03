8f83c66be4ba2a61316642211f2c7af28dac974c
Mohammed D. Belgoumri <mohammedbelgoumri@gmail.com>
2021-07-03 12:59:06 +0100

    Tested printLastUsed(): Success

--- source/search.c
+++ source/search.c
@@ -41,8 +41,8 @@ int main(int argc, char *argv[])
         printVersion();
     if (fall)
         fdate = fsize = ftype = fprotection = 1;
-    // printHeader(fdate, fmodification, fprotection, fsize, ftype);
+    printHeader(1, fmodification, fprotection, fsize, ftype);
     // printf("%-12s%-12s%-6s%-12s%-15s%s\n", "directory", "777", "4096", "03-07-2021", "03-07-2021", "functions");
-    printDirectory(argv[1], argv[2], fdate, fmodification, fprotection, fsize, ftype, depth);
+    printDirectory(argv[1], argv[2], 1, fmodification, fprotection, fsize, ftype, depth);
     return EXIT_SUCCESS;
 }
