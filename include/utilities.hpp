3b6a431b23038b224c95c674f4bdcb9dde5d3f92
Mohammed Belgoumri <mohammedbelgoumri@gmail.com>
2021-07-26 17:09:27 +0100

    Remake in C++: Initial commit

---
+++ include/utilities.hpp
@@ -0,0 +1,4 @@
+#ifndef __UTILITIES__HPP__
+#define __UTILITIES__HPP__
+
+#endif // __UTILITIES__HPP__
---
+++ source/main.cpp
@@ -0,0 +1,9 @@
+#include "include/utilities.hpp"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    return EXIT_SUCCESS;
+}
